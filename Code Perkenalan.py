efa75b82b80c40962172e6b6cd184c6493a98d0c
Nalendraputra <145786217+Nalendraputra@users.noreply.github.com>
2023-09-25 16:13:08 +0700

    Add files via upload

---
+++ Code Perkenalan.py
@@ -0,0 +1,6 @@
+ratna = (input("Nama kamu siapa?"))
+ratna = (input("Darimana asal  kamu Nalendra?"))
+ratna = (input("Sebelumnya bersekolah dimana?"))
+ratna = (input("hallo selamat siang Nalendra"))
+ratna = (input("Wah dari Bekasi"))
+ratna = (input("Pasti menyenangkan bersekolah di Sma 3 Babelan"))
---
+++ Kodee.py
@@ -0,0 +1,11 @@
+print ('Hitung luas ruangan')
+panjang = float(input('Masukan panjang ruangan : '))
+lebar = float(input('Masukan lebar ruangan :'))
+meter = (input('Masukan Satuan : '))
+rumus = (panjang*lebar)
+
+print ("Jadi hasilnya", + rumus, "Meter")
+
+              
+
+
